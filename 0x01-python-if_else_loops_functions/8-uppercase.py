345960651686e7909e21771d36796de775ddb929
Oke Edafe Great <greatedafeoke@gmail.com>
2021-09-07 05:12:26 -0700

    Created 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in str:
+        val = ord(i)
+        char = ""
+        if (val >= 97) and (val <= 122):
+            char = chr(val - 32)
+        else:
+            char = i
+        print(char, end="")
+    print("")
