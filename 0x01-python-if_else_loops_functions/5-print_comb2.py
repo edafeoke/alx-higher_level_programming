e0375d98c91840fe65a56c4556dc3886d1be0687
Oke Edafe Great <greatedafeoke@gmail.com>
2021-09-07 04:28:00 -0700

    Fixed requirements condition for 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(10):
-    for j in range(10):
-        if not (i == 9 and j == 9):
-            print("{}{}".format(i, j), end=", ")
-        else:
-            print("{}{}".format(i, j))
+for i in range(100):
+    if not (i == 99):
+        print("{:2}".format(i), end=", ")
+    else:
+        print("{:2}".format(i))
