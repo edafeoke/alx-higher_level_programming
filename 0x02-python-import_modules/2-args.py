a9f5deaf99046d9f9f832efe16414bc92837f645
Oke Edafe Great <greatedafeoke@gmail.com>
2021-09-09 14:56:23 -0700

    Corrected typo error in 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     length = len(sys.argv) - 1
     if length == 0:
         print("0 arguments.")
-    elif length == :
+    elif length == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(length))
