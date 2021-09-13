f9be7f9a41148dd2d243d7b1a953b755f0bf24f5
Oke Edafe Great <greatedafeoke@gmail.com>
2021-09-12 22:35:12 -0700

    Fixed bug in 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        e = " "
         for j in range(len(i)):
-            if j == len(i) - 1:
-                e = '\n'
-            print("{:d}".format(i[j]), end=e)
+            print("{:d}".format(i[j]), end="")
+            if j != len(i) - 1:
+                print(" ", end="")
+        print("")
