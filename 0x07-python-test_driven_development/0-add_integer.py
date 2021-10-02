045d569c0dc0103277c7879b178c693d104442db
Oke Edafe Great <greatedafeoke@gmail.com>
2021-10-02 16:10:31 -0700

    Added 0-add_integer.py, tests/0-add_integer.txt

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+add_integer module that add two number
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers
+
+    Args
+        a: an integer or float
+        b: an integer or float
+    """
+    t = [int, float]
+    if (type(a) not in t) or (type(b) not in t):
+        raise TypeError("a must be an integer")
+    return (int(a) + int(b))
