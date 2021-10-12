7035432062126c8b4290ed059e381464555a8822
Oke Edafe Great <greatedafeoke@gmail.com>
2021-10-12 04:32:44 -0700

    Fixed bugs in task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
     that inherited (directly or indirectly) from the specified class
     otherwise False
     '''
-    return isinstance(type(obj), a_class) and (type(obj) != a_class)
+    return issubclass(type(obj), a_class) and (type(obj) != a_class)
