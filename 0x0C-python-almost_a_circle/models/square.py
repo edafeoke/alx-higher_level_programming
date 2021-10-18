6632b4f3541666399ae3f52b302b234777572855
Oke Edafe Great <greatedafeoke@gmail.com>
2021-10-18 13:18:32 -0700

    Completed all tasks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,81 +1,49 @@
 #!/usr/bin/python3
-"""
-Module Square
-Inherits from class Rectangle,
-That inherits from class Base
-"""
-
-
+'''Module for Square class.'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """class Square inherits from class Rectangle
-    Methods:
-        def __init__(self,size,x,y,id)
-        def __str__(self)
-        def update(self, *args, **kwargs)
-        def to_dictionary(self)
-    Getter:
-       def size(self)
-    Setter:
-        def size(self, value)
-    """
+    '''A Square class.'''
+
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialization
-        calls the supper class rectangle
-        and assigns width and height to size
-        """
+        '''Constructor.'''
         super().__init__(size, size, x, y, id)
-        self.size = size
+
+    def __str__(self):
+        '''Returns string info about this square.'''
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """Size getter"""
+        '''Size of this square.'''
         return self.width
 
     @size.setter
     def size(self, value):
-        """size setter"""
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """Overrides to return
-        "[Square] (<id>) <x>/<y> - <size>"
-        """
-        return ("[Square] ({}) {}/{} - {}".format
-                (self.id, self.x, self.y, self.size))
+    def __update(self, id=None, size=None, x=None, y=None):
+        '''Internal method that updates instance attributes via */**args.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
 
     def update(self, *args, **kwargs):
-        """Assign arguments to attributes"""
+        '''Updates instance attributes via no-keyword & keyword args.'''
         if args:
-            for i, j in enumerate(args):
-                if i == 0:
-                    self.id = j
-                elif i == 1:
-                    self.size = j
-                elif i == 2:
-                    self.x = j
-                else:
-                    self.y = j
-
-        else:
-            """kwargs"""
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "size" in kwargs:
-                self.size = kwargs["size"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            if "y" in kwargs:
-                self.y = kwargs["y"]
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
 
     def to_dictionary(self):
-        """returns the dictionary representation of square"""
-        dic = {}
-        dic["id"] = self.id
-        dic["size"] = self.size
-        dic["x"] = self.x
-        dic["y"] = self.y
-        return dic
+        '''Returns dictionary representation of this class.'''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
