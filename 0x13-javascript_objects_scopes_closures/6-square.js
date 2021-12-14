ce0f78ededc5f4e1a4e70be3bf6c3135b936a23b
Oke Edafe Great <greatedafeoke@gmail.com>
2021-12-14 07:53:44 -0800

    Added task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+let SquareS = require('./5-square.js');
+
+class Square extends SquareS {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.size; i++) {
+        let string = '';
+        for (let j = 0; j < this.size; j++) {
+	  string += c;
+	}
+        console.log(string);
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+module.exports = Square;
