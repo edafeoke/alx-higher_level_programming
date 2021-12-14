628e985deaa2a57680f17843409373ca8cd40234
Oke Edafe Great <greatedafeoke@gmail.com>
2021-12-14 07:59:16 -0800

    Corrected task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,14 +5,13 @@ const SquareS = require('./5-square');
 class Square extends SquareS {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 
   charPrint (c) {
     if (c) {
-      for (let i = 0; i < this.size; i++) {
+      for (let i = 0; i < this.height; i++) {
         let string = '';
-        for (let j = 0; j < this.size; j++) {
+        for (let j = 0; j < this.width; j++) {
           string += c;
         }
         console.log(string);
