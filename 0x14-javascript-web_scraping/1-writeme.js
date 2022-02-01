1bd3bef33d01f01be2bfd9cb7e04c6159a6e5a56
Oke Edafe Great <greatedafeoke@gmail.com>
2022-02-01 13:59:48 -0800

    Added 1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * reads and prints the content of a file
+ * first argument is the file path
+ */
+const myArgs = process.argv.slice(2);
+const fs = require('fs');
+fs.writeFile(myArgs[0], myArgs[1],
+  {
+    encoding: "utf-8",
+    flag: "w"
+  },
+  (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
