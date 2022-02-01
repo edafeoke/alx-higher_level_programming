f35ab7bfa575cff13e51ad3575eab85d6d8dbf92
Oke Edafe Great <greatedafeoke@gmail.com>
2022-02-01 14:04:27 -0800

    Added 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,11 +7,11 @@ const myArgs = process.argv.slice(2);
 const fs = require('fs');
 fs.writeFile(myArgs[0], myArgs[1],
   {
-    encoding: "utf-8",
-    flag: "w"
+    encoding: 'utf-8',
+    flag: 'w'
   },
   (error) => {
-  if (error) {
-    console.log(error);
-  }
-});
+    if (error) {
+      console.log(error);
+    }
+  });
